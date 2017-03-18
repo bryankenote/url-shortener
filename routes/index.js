1b5d6d371ee79bbf38cbb35bfc938a4c96482247
Bryan <brykenote@gmail.com>
2017-03-18 01:45:17 +0000

    delete short url if already in use

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,13 @@ router.get('/all', function (req, res) {
     });
 });
 
+router.delete('/quotes', function (req, res) {
+    var db = req.db;
+    var id = req.body.id;
+    
+    
+});
+
 router.get('/:protocol//:url', function (req, res) {
     var db = req.db;
     var protocol = req.params.protocol;
@@ -26,18 +33,24 @@ router.get('/:protocol//:url', function (req, res) {
       res.end('Invalid protocol in url');
     } else {
       
-      var createData = {
-          "url": protocol + '//' + url,
-          "urlNum": String(getRandomNumber(1000, 9999))
-      };
-      var collection = db.get('urlcollection');
-      collection.insert(createData, (err, doc) => {
-          if (err) {
-              res.send("there was a problem saving the url in the database");
-          } else {
-              res.render('all', {'urls': [createData]});
-          }
-      });
+        var collection = db.get('urlcollection');
+        var createData = {
+            "url": protocol + '//' + url,
+            "urlNum": String(getRandomNumber(1000, 9999))
+        };
+        collection.findOneAndDelete(
+            { 'urlNum': createData.urlNum },
+            function (err, result) {
+                if (err)
+                    res.render('error', {'error':'invalid url'});
+                collection.insert(createData, function (err, doc) {
+                    if (err)
+                        res.send("there was a problem saving the url in the database");
+                    else
+                        res.render('all', {'urls': [createData]});
+                });
+            }
+        );
     }
 });
 
