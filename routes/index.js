52feeedcd17ba83601ed8f02629f2a7838d94e92
Bryan <brykenote@gmail.com>
2017-03-18 01:12:20 +0000

    add urls to db

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,32 @@ router.get('/all', function (req, res) {
     });
 });
 
+router.get('/:protocol//:url', function (req, res) {
+    var db = req.db;
+    var protocol = req.params.protocol;
+    var url = req.params.url;
+    if (protocol !== 'http:' && protocol !== 'https:') {
+      res.end('Invalid protocol in url');
+    } else {
+      
+      var createData = {
+          "url": protocol + url,
+          "urlNum": getRandomNumber(1000, 9999)
+      };
+      var collection = db.get('urlcollection');
+      collection.insert(createData, (err, doc) => {
+          if (err) {
+              res.send("there was a problem saving the url in the database");
+          } else {
+              //res.send(doc);
+              res.render('all', {'urls': [createData]});
+          }
+      });
+    }
+});
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 module.exports = router;
