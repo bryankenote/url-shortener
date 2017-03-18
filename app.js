d4afad1852d3a4048452a02655e5f5d24bf5192f
Bryan <brykenote@gmail.com>
2017-03-18 01:02:46 +0000

    init mongodb

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/url-shortener');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -27,6 +31,12 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', index);
 app.use('/users', users);
