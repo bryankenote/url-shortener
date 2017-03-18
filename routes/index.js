d5ca6b0060d0daa1949805599df17039deedd53f
Bryan <brykenote@gmail.com>
2017-03-18 01:03:05 +0000

    render all urls

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/all', function (req, res) {
+    var db = req.db;
+    var collection = db.get('urlcollection');
+    collection.find({},{},function(err,docs){
+        if (err) {
+            res.render('error', {'error': err});
+        } else {
+            res.render('all', {'urls': docs});
+        }
+    });
 });
 
 module.exports = router;
