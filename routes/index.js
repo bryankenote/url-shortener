8323d323bbed1384110394496490b13e8a8b9bfd
Bryan <brykenote@gmail.com>
2017-03-18 01:34:24 +0000

    redirect to url in db

--- routes/index.js
+++ routes/index.js
@@ -27,18 +27,36 @@ router.get('/:protocol//:url', function (req, res) {
     } else {
       
       var createData = {
-          "url": protocol + url,
-          "urlNum": getRandomNumber(1000, 9999)
+          "url": protocol + '//' + url,
+          "urlNum": String(getRandomNumber(1000, 9999))
       };
       var collection = db.get('urlcollection');
       collection.insert(createData, (err, doc) => {
           if (err) {
               res.send("there was a problem saving the url in the database");
           } else {
-              //res.send(doc);
               res.render('all', {'urls': [createData]});
           }
       });
+    }
+});
+
+router.get('/:newUrl', function (req, res) {
+    var newUrl = req.params.newUrl;
+    if (isNaN(newUrl)) {
+        res.render('error', {'error':'invalid url'});
+    } else {
+        var db = req.db;
+        var collection = db.get('urlcollection');
+        collection.find({
+            'urlNum': newUrl
+        },{},function(err,docs){
+            if (err) {
+                res.end("The url you entered wasn't created with this app.");
+            } else {
+                res.redirect(docs[0].url);
+            }
+        });
     }
 });
 
